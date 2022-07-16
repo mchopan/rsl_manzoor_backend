c6d512fedc676bb791f4c8d89ab51d71a2f089f0
Manzoor_Chopan <mchopan21@gmail.com>
2022-07-16 18:12:04 +0530

    added get method

--- src/main/java/com/project_news/Service/NewsService.java
+++ src/main/java/com/project_news/Service/NewsService.java
@@ -2,7 +2,13 @@ package com.project_news.Service;
 
 import com.project_news.Entity.News;
 
+import java.util.ArrayList;
+
 
 public interface NewsService {
-    News saveNews(News news);
+    public News saveNews(News news);
+
+    public  ArrayList<News> getNews();
+
+    public News getNewsById(Long newsId);
 }
--- src/main/java/com/project_news/Service/NewsServiceImpl.java
+++ src/main/java/com/project_news/Service/NewsServiceImpl.java
@@ -5,6 +5,8 @@ import com.project_news.Repository.NewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class NewsServiceImpl implements NewsService  {
 
@@ -13,5 +15,15 @@ public class NewsServiceImpl implements NewsService  {
     @Override
     public News saveNews(News news) {
         return newsRepository.save(news);
+    }
+
+    @Override
+    public ArrayList<News> getNews() {
+        return (ArrayList<News>) newsRepository.findAll();
+    }
+
+    @Override
+    public News getNewsById(Long newsId) {
+        return newsRepository.findById(newsId).get();
     }
 }
